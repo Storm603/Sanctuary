991a494401b1f11cc0aff610d97e75c57d7e01bc
Storm603 <kstoyanovwork@abv.bg>
2022-11-01 21:57:51 +0200

    Add project files.

---
+++ Sanctuary.Web/Controllers/AccountController.cs
@@ -0,0 +1,103 @@
+﻿using System.Security;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Sanctuary.Data.Models;
+using Sanctuary.Web.Data;
+using Sanctuary.Web.Views.ViewModels;
+
+namespace Sanctuary.Web.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private UserManager<ApplicationUser> UserManager { get; set; }
+        private SignInManager<ApplicationUser> SignInManager { get; set; }
+        private ApplicationDbContext ApplicationDbContext { get; set; }
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, ApplicationDbContext applicationDbContext)
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+            ApplicationDbContext = applicationDbContext;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = new ApplicationUser()
+            {
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                Email = model.Email,
+                UserName = model.Email
+            };
+
+            var creationResult = await UserManager.CreateAsync(user, model.Password);
+
+            if (creationResult.Succeeded)
+            {
+                return RedirectToAction("LogIn");
+            }
+
+            foreach (IdentityError error in creationResult.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(LogInViewModel userLogInModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(userLogInModel);
+            }
+
+            var user = await UserManager.FindByEmailAsync(userLogInModel.Email);
+
+            var resultFromLogIn = await SignInManager.PasswordSignInAsync(user, userLogInModel.Password, isPersistent: false, lockoutOnFailure: false);
+
+            if (resultFromLogIn.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError("", "Invalid login");
+;
+            return View(userLogInModel);
+        }
+
+        public async Task<IActionResult> LogOut()
+        {
+            await SignInManager.SignOutAsync();
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ Sanctuary.Web/Views/ViewModels/LogInViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sanctuary.Web.Views.ViewModels
+{
+    public class LogInViewModel
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string? Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+    }
+}
